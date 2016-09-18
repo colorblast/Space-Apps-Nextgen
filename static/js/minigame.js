a9126f407ea3a00084c2391ec2be44a8ab855146
srafi1 <srafi1@stuy.edu>
2016-09-18 09:56:08 -0400

    idkman

--- static/js/minigame.js
+++ static/js/minigame.js
@@ -1,16 +1,59 @@
 var lat, lng, category, loop;
-var canvas, ctx;
+var canvas, ctx, bgimg;
 var count = 0;
+var ob1, ob2;
+var charX, charY;
+var up = false, down = false, left = false, right = false;
 
 function initGame(nlat, nlng, ncategory) {
 	canvas = document.getElementById('minigameCanvas');
 	ctx = canvas.getContext('2d');
 	lat = nlat;
 	lng = nlng;
+	category = ncategory;
+
+	charX = 250;
+	charY = 250;
+	
+	$(function() {
+		$.getJSON('https://api.nasa.gov/planetary/earth/imagery?lon='+lng+'&lat='+lat+'&date=2014-02-01&cloud_score=True&api_key=YiSsr6JNgEpcszd0LoaIWsR4yGEv4OqemQMkwOMm',function(data) {
+			var bgurl = data['url'];
+			bgimg = new Image;
+			bgimg.src = bgurl;
+		});
+	});
+	
+	ob1 = {
+		img: null,
+		x: 100,
+		y: 100,
+		touch: false
+	};
+	ob2 = {
+		img: null,
+		x: 300,
+		y: 350,
+		touch: false
+	};
+	console.log("Wildfires " + category);
+	if (category == "Wildfires") {
+		ob1.img = new Image;
+		ob1.img.src = '/img/fire.png';
+		ob2.img = new Image;
+		ob2.img.src = '/img/fire.png';
+	}
+
+	canvas.addEventListener('keypress', keydown, false);
+	
 	loop = setInterval(update, 17);
 }
 
 function update() {
+	if (down) charY++;
+    if (up) charY--;
+    if (right) charX++;
+    if (left) charX--;
+
 	count++;
 	if (count > 200) {
 		clearInterval(loop);
@@ -20,6 +63,31 @@ function update() {
 }
 
 function draw() {
-	ctx.fillStyle = "#ff0000";
-	ctx.fillRect(0, 0, 500, 500);
+	ctx.fillStyle = "#ffffff";
+	ctx.fillRect(0, 0, 512, 512);
+	ctx.drawImage(bgimg, 0, 0);
+	if (!ob1.touch) ctx.drawImage(ob1.img, ob1.x, ob1.y, 100, 140);
+	if (!ob2.touch) ctx.drawImage(ob2.img, ob2.x, ob2.y, 100, 140);
+	ctx.fillRect(charX, charY, 20, 20);
+	console.log(charX + ", " + charY);
+}
+
+function keydown(e) {
+if ( e.keyCode == 37 ) {
+	left = true;
+}
+
+if ( e.keyCode == 38 ) {
+	up = true;
+}
+
+if ( e.keyCode == 39 ) {
+	right = true;
+}
+
+if ( e.keyCode == 40 ) {
+	down = true;
+}
+
+console.log(charX + ", " + charY);
 }
